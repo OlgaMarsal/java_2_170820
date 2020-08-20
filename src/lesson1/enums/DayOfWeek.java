57512057af91b5577e92b248dc83cd99492b9979
OlgaMArsal <inconcretehome@gmail.com>
2020-08-20 15:34:02 +0300

    1 - java_2_170820

--- src/lesson1/enums/DayOfWeek.java
+++ src/lesson1/enums/DayOfWeek.java
@@ -1,7 +1,7 @@
 package lesson1.enums;
 
 public enum DayOfWeek {
-    Monday(8), Tuesday(8), Wednesday(8), Thursday(8), Friday(8), Saturday(0), Sunday(0);
+    Monday(8), Tuesday(8), Wednesday(8), Thursday(8), Friday(7), Saturday(0), Sunday(0);
 
     private int hours;
 
